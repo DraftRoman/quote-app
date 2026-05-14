4208de0e359074eda1e76af5f7926b586479e772
Roman <roman.boxes@gmail.com>
2026-05-14 13:51:37 +0200

    Update config.js
    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 
-BACKEND_API = "https://roman-quote-app-backend.178.105.39.91.sslip.io";
+const BACKEND_API = "https://roman-quote-app-backend.178.105.39.91.sslip.io";
 export default BACKEND_API;
 
